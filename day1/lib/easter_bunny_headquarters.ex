40b433cfbdacc1d123e1de8def3463dcafc7ee3a
Karl Eklund <localpart@gmail.com>
2016-12-05 14:00:48 +0100

    refactor

--- day1/lib/easter_bunny_headquarters.ex
+++ day1/lib/easter_bunny_headquarters.ex
@@ -1,35 +1,4 @@
 defmodule EasterBunnyHeadquarters do
-  @moduledoc """
-  --- Day 1: No Time for a Taxicab ---
-
-  Santa's sleigh uses a very high-precision clock to guide its movements, and the clock's oscillator
-  is regulated by stars. Unfortunately, the stars have been stolen... by the Easter Bunny. To save
-  Christmas, Santa needs you to retrieve all fifty stars by December 25th.
-
-  Collect stars by solving puzzles. Two puzzles will be made available on each day in the advent
-  calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star.
-  Good luck!
-
-  You're airdropped near Easter Bunny Headquarters in a city somewhere. "Near", unfortunately, is as
-  close as you can get - the instructions on the Easter Bunny Recruiting Document the Elves
-  intercepted start here, and nobody had time to work them out further.
-
-  The Document indicates that you should start at the given coordinates (where you just landed) and
-  face North. Then, follow the provided sequence: either turn left (L) or right (R) 90 degrees, then
-  walk forward the given number of blocks, ending at a new intersection.
-
-  There's no time to follow such ridiculous instructions on foot, though, so you take a moment and
-  work out the destination. Given that you can only walk on the street grid of the city, how far is
-  the shortest path to the destination?
-
-  For example:
-
-  Following R2, L3 leaves you 2 blocks East and 3 blocks North, or 5 blocks away.
-  R2, R2, R2 leaves you 2 blocks due South of your starting position, which is 2 blocks away.
-  R5, L5, R5, R3 leaves you 12 blocks away.
-  How many blocks away is Easter Bunny HQ?
-  """
-
   @doc """
   iex> EasterBunnyHeadquarters.distance("")
   0
@@ -61,15 +30,34 @@ defmodule EasterBunnyHeadquarters do
     abs(x) + abs(y)
   end
 
-  defp destination(position = {_, _, _}, []),         do: position
-  defp destination({:north, x, y}, [{:r, n} | rest]), do: destination({:east,  x + n, y    }, rest)
-  defp destination({:north, x, y}, [{:l, n} | rest]), do: destination({:west,  x - n, y    }, rest)
-  defp destination({:east,  x, y}, [{:r, n} | rest]), do: destination({:south, x,     y - n}, rest)
-  defp destination({:east,  x, y}, [{:l, n} | rest]), do: destination({:north, x,     y + n}, rest)
-  defp destination({:west,  x, y}, [{:r, n} | rest]), do: destination({:north, x,     y + n}, rest)
-  defp destination({:west,  x, y}, [{:l, n} | rest]), do: destination({:south, x,     y - n}, rest)
-  defp destination({:south, x, y}, [{:r, n} | rest]), do: destination({:west,  x - n, y    }, rest)
-  defp destination({:south, x, y}, [{:l, n} | rest]), do: destination({:east,  x + n, y    }, rest)
+  defp destination(position = {_, _, _}, []), do: position
+  defp destination({direction, x, y}, [{spin, steps} | rest]) do
+    direction = turn(direction, spin)
+    {x, y} = move(direction, x, y, steps)
+    destination({direction, x, y}, rest)
+  end
+
+  defp turn(direction, :r) do
+    case direction do
+      :north -> :east
+      :east -> :south
+      :south -> :west
+      :west -> :north
+    end
+  end
+  defp turn(direction, :l) do
+    case direction do
+      :north -> :west
+      :west -> :south
+      :south -> :east
+      :east -> :north
+    end
+  end
+
+  defp move(:north, x, y, steps), do: {x + steps, y}
+  defp move(:east, x, y, steps), do: {x, y + steps}
+  defp move(:south, x, y, steps), do: {x - steps, y}
+  defp move(:west, x, y, steps), do: {x, y - steps}
 
   defp parse_step(<<char :: utf8>> <> steps_string) do
     {String.to_atom(String.downcase(List.to_string([char]))),
